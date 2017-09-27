afa65d96d11abb726bf57c81da13196955155fcc
cdaringe <christopher.dieringer@gmail.com>
2017-09-27 12:24:01 -0700

    stable-db-build

--- packages/db/src/migrate.js
+++ packages/db/src/migrate.js
@@ -6,18 +6,34 @@ const execa = require('execa')
 const bb = require('bluebird')
 
 async function migrate () {
+  let migrated = false
   console.log('booting server')
-  const server = execa('node', ['/db/node_modules/.bin/pouchdb-server'], {
-    env: Object.assign({}, process.env, { DEBUG: '*' })
-  })
+  const server = execa(
+    'node',
+    [
+      '/db/node_modules/.bin/pouchdb-server',
+      '--level-backend',
+      'sqldown'
+    ],
+    {
+      env: Object.assign({}, process.env, { DEBUG: '' })
+    }
+  )
   server.stdout.pipe(process.stdout)
-  await bb.delay(10000)
+  server.stderr.pipe(process.stderr)
+  server.catch(err => {
+    if (migrated) return
+    console.error(err)
+    process.exit(1)
+  })
+  await bb.delay(6000)
   const db = new Pouchy({ url: 'http://127.0.0.1:5984/lang' })
   await db.save({ 'name': 'javascript', 'is': 'easy' })
   await db.save({ 'name': 'rust', 'is': 'safe' })
   await db.save({ 'name': 'erlang', 'is': 'interesting' })
   await db.save({ 'name': 'java', 'is': 'not cool anymore' })
-  console.log(`added: ${await db.all()}`)
+  console.log(`added: ${(await db.all()).length} docs`)
+  migrated = true
   server.kill()
 }
 
